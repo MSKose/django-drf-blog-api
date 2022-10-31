cb8cd311b8b520158fb33c27a4d426b35751c82d
MSKose <saitmustafa1998@gmail.com>
2022-10-31 20:53:02 +0100

    still getting username already exists error

--- users/views.py
+++ users/views.py
@@ -27,20 +27,20 @@ class RegisterView(generics.CreateAPIView):
 class ProfileView(LoginRequiredMixin, RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileUpdateForm
-    http_method_names = ['get', 'patch', 'delete']
+    # http_method_names = ['get', 'patch', 'delete']
 
-    def update(self, request, *args, **kwargs):
-        # partial = True
-        instance = self.get_object()
-        serializer = self.get_serializer(instance, data=request.data, partial=True)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+    # def update(self, request, *args, **kwargs):
+    #     # partial = True
+    #     instance = self.get_object()
+    #     serializer = self.get_serializer(instance, data=request.data, partial=True)
+    #     serializer.is_valid(raise_exception=True)
+    #     self.perform_update(serializer)
 
-        return Response(serializer.data)
+    #     return Response(serializer.data)
 
-    def perform_update(self, serializer):
-        serializer.save()
+    # def perform_update(self, serializer):
+    #     serializer.save()
 
-    def partial_update(self, request, *args, **kwargs):
-        kwargs['partial'] = True
-        return self.update(request, *args, **kwargs)
+    # def partial_update(self, request, *args, **kwargs):
+    #     kwargs['partial'] = True
+    #     return self.update(request, *args, **kwargs)
